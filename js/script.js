d0d30fe9a1372f2030837a774f400cfa3b863a61
BenEkstrand <lachdenan@byui.edu>
2022-09-17 15:45:38 -0700

    Commit 2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// footer
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+document.querySelector("#lu").textContent = `Last Updated:  ${document.lastModified}`;
+
+// custom
+let myName = document.querySelector(".myName");
+myName.textContent = "Ben Ekstrand";
+let myloc = document.querySelector(".myloc");
+myloc.textContent = "Spokane, WA";
+let theTitle = document.querySelector("h1").textContent;
+document.querySelector("title").textContent = `${theTitle}: ${myName}`;
