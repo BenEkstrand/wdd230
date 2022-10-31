1276c06a41593e824aae4b7cca09c9c8fd2ef177
BenEkstrand <lachdenan@byui.edu>
2022-10-31 14:16:41 -0700

    Last minute edits

--- week07/chamber/scripts/dates.js
+++ week07/chamber/scripts/dates.js
@@ -1,2 +1,16 @@
+//Banner
+let banner = document.getElementById("banner");
+let dayOfWeek = date.getDay();
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    banner.style.display = "block";
+} else {
+    banner.style.display = "none";
+}
+
+//Last site update//
+const lastUpdate = document.querySelector("#lastUpdate");
+lastUpdate.innerHTML = date.toLocaleDateString("en-UK", options);
+
+//Copyright//
 const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
--- week07/chamber/scripts/menu.js
+++ week07/chamber/scripts/menu.js
@@ -4,18 +4,18 @@ function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+//  select the elements to manipulate (output to)
+ const datefield = document.querySelector(".date");
+ const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
-// derive the current date using a date object
+ // derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
+ 	now
+ );
+ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+ 	dateStyle: "full"
+ }).format(now);
+ // long, medium, short options ... try them
 
-datefield.innerHTML = `<em>${fulldate}</em>`;
+ datefield.innerHTML = `<em>${fulldate}</em>`;
